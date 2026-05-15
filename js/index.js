eb269257b6dab59b9d04d38c6888da9ceab70131
Rochelle <rochellecelesta17@gmail.com>
2026-05-14 20:27:07 -0400

    fixed some minor mistakes

--- js/index.js
+++ js/index.js
@@ -9,9 +9,9 @@ const copyright = document.createElement('p');
 copyright.innerHTML = `Rochelle Celesta &copy; ${thisYear}`;
 footer.appendChild(copyright);
 
-skills = ["JavaScript", "HTML", "CSS", "Python"];
-skillsSection = document.getElementById('Skills');
-skillsList = skillsSection.querySelector('ul');
+const skills = ["JavaScript", "HTML", "CSS", "Python"];
+const skillsSection = document.getElementById('Skills');
+const skillsList = skillsSection.querySelector('ul');
 
 for (let i = 0; i < skills.length; i++) {
     const skill = document.createElement('li');
