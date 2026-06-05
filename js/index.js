d176fc5f44475b80f24240b089edaa7743bd392f
Rochelle <rochellecelesta17@gmail.com>
2026-06-04 22:10:18 -0400

    API fetch completed

--- js/index.js
+++ js/index.js
@@ -53,3 +53,23 @@ messageForm.addEventListener('submit', function(event) {
     messageForm.reset();
 
 });
+
+fetch(https:/api.github.com/users/urcelestial/repos)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(repositories) {
+        console.log("repositories:", repositories);
+
+    .catch(function(error) {
+        console.log("Error fetching repositories:", error);
+    });
+        const projectSection = document.getElementById('projects');
+        const projectList = projectSection.querySelector('ul');
+
+        for (let i = 0; i < repositories.length; i++) {
+            const project = document.createElement('li');
+            project.innerText = repositories[i].name;
+            projectList.appendChild(project);
+        }
+    });
